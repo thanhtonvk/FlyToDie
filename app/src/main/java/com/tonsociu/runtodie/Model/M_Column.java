d72783bc1c9e167d7fc6d6929a6323126f4648c0
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2021-04-21 16:52:47 +0700

    Initial commit

---
+++ app/src/main/java/com/tonsociu/runtodie/Model/M_Column.java
@@ -0,0 +1,28 @@
+package com.tonsociu.runtodie.Model;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import com.tonsociu.runtodie.R;
+
+public class M_Column {
+    public Bitmap col;
+   public int x=0, y, width, height;
+
+    public M_Column(Resources resources) {
+        col = BitmapFactory.decodeResource(resources, R.drawable.col);
+        width = col.getWidth();
+        height = col.getHeight();
+        col = Bitmap.createScaledBitmap(col,width,height,false);
+        y = -height;
+    }
+
+    public Rect getCollisionShapeCol() {
+        return new Rect(x, y,x+width,y+height );
+    }
+    public Bitmap getCol(){
+        return col;
+    }
+}
